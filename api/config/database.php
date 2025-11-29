a6dadf1897ec5236f3a2bcce4f64159da7364f6f
Kraids99 <alexanderwongso99@gmail.com>
2025-11-29 22:54:16 +0700

    commit model users

--- api/config/database.php
+++ api/config/database.php
@@ -11,5 +11,8 @@ $database = $env['DB_NAME'];
 $conn = mysqli_connect($hostname, $username, $password, $database);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    // die("Connection failed: " . mysqli_connect_error());
+    error_log(mysqli_connect_error());
+    echo "Database sedang bermasalah.";
+    exit;
 }
---
+++ api/models/User.php
@@ -0,0 +1,128 @@
+<?php
+
+// s = string
+// i = integer
+// d = double	
+// b = blob	
+
+class User 
+{
+    private $db;
+    private $table = "users";
+
+    public function __construct($conn)
+    {
+        $this->db = $conn;
+    }
+
+     // create user
+    public function insertUser($username, $email, $password, $role='user')
+    {
+        $query = "INSERT INTO {$this->table} (username, email, password, role) VALUES (?, ?, ?, ?)";
+
+        // siapin query
+        $stmt = mysqli_prepare($this->db, $query);
+
+        // ngisi data 
+        mysqli_stmt_bind_param($stmt, "ssss", $username, $email, $password, $role);
+        
+        // jalankan query
+        return mysqli_stmt_execute($stmt);
+    }
+
+    public function updateUser($id, $username, $email)
+    {
+        $query = "UPDATE {$this->table} SET username = ?, email = ? WHERE user_id = ?";
+
+        // siapin query
+        $stmt = mysqli_prepare($this->db, $query);
+
+        // ngisi data
+        mysqli_stmt_bind_param($stmt, "ssi", $username, $email, $id);
+
+        // jalankan query
+        return mysqli_stmt_execute($stmt);
+    }
+
+    public function updateProfilePicture($id, $fileName)
+    {
+        $query = "UPDATE {$this->table} SET user_profile = ? WHERE user_id = ?";
+
+        // siapin query
+        $stmt = mysqli_prepare($this->db, $query);
+
+        // ngisi data
+        mysqli_stmt_bind_param($stmt, "si", $fileName, $id);
+
+        // jalankan query
+        return mysqli_stmt_execute($stmt);
+    }
+
+    public function updatePassword($id, $password)
+    {
+        $query = "UPDATE {$this->table} SET password = ? WHERE user_id = ?";
+
+        // siapin query
+        $stmt = mysqli_prepare($this->db, $query);
+
+        // ngisi data
+        mysqli_stmt_bind_param($stmt, "si", $password, $id);
+
+        // jalankan query
+        return mysqli_stmt_execute($stmt);
+    }
+
+    public function findByEmail($email)
+    {
+        $query = "SELECT * FROM users WHERE email=?";
+        // siapin query
+        $stmt = mysqli_prepare($this->db, $query);
+
+        // ngisi data email
+        mysqli_stmt_bind_param($stmt, "s", $email); 
+        
+        // jalankan query
+        mysqli_stmt_execute($stmt);
+
+        // ambil hasil dari database
+        $result = mysqli_stmt_get_result($stmt);
+        
+        // ambil data 1 baris sebagai array
+        return mysqli_fetch_assoc($result);
+    }
+   
+
+    public function getById($id)
+    {
+        $query = "SELECT * FROM {$this->table} WHERE user_id = ?";
+
+        // siapin query
+        $stmt = mysqli_prepare($this->db, $query);
+
+        // ngisi data
+        mysqli_stmt_bind_param($stmt, "i", $id);
+
+        // jalankan query
+        mysqli_stmt_execute($stmt);
+
+        // ambil hasil dari database
+        $result = mysqli_stmt_get_result($stmt);
+
+        // ambil data 1 baris sebagai array
+        return mysqli_fetch_assoc($result);
+    }
+
+    public function deleteUser($id)
+    {
+        $query = "DELETE FROM {$this->table} WHERE user_id = ?";
+
+        // siapin query
+        $stmt = mysqli_prepare($this->db, $query);
+
+        // ngisi data
+        mysqli_stmt_bind_param($stmt, "i", $id);
+
+        // jalankan query
+        return mysqli_stmt_execute($stmt);
+    }
+}
