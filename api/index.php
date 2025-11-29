53794fcc71758f8259a5ee069590ef81f3ab651b
kursiterbang <230712512@students.uajy.ac.id>
2025-11-30 02:36:58 +0700

    make index.php

--- api/index.php
+++ api/index.php
@@ -1,8 +1,11 @@
 <?php
 require_once __DIR__ . '/config/database.php';
-require_once __DIR__ . '/controllers/Auth/RegisterController.php';
-require_once __DIR__ . '/controllers/Auth/LoginController.php';
-require_once __DIR__ . '/controllers/Auth/LogoutController.php';
+require_once __DIR__ . '/controllers/auth/RegisterController.php';
+require_once __DIR__ . '/controllers/auth/LoginController.php';
+require_once __DIR__ . '/controllers/auth/LogoutController.php';
+require_once __DIR__ . '/controllers/TicketController.php';
+require_once __DIR__ . '/controllers/TransactionController.php';
+require_once __DIR__ . '/controllers/EventController.php';
 
 switch ($action) {
     case 'register':
@@ -36,6 +39,42 @@ switch ($action) {
 
     case 'event_delete':
         $event->delete();
+    break;
+
+    case 'ticket':
+        $ticket->index();
+    break;
+
+    case 'ticket_show':
+        $ticket->show();
+    break;
+
+    case 'ticket_create':
+        $ticket->store();
+    break;
+
+    case 'ticket_update':
+        $ticket->update();
+    break;
+
+    case 'ticket_delete':
+        $ticket->delete();
+    break;
+
+    case 'transaction':
+        $transaction->index();
+    break;
+
+    case 'transaction_show':
+        $transaction->show();
+    break;
+    
+    case 'transaction_create':
+        $transaction->store();
+    break;
+
+    case 'transaction_delete':
+        $transaction->delete();
     break;
 
     default:
