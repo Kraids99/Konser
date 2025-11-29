da2ab80cfb891dffd7a12dde4f2f9bb013cffcd9
Kraids99 <alexanderwongso99@gmail.com>
2025-11-30 01:39:52 +0700

    Update EventController.php

--- api/controllers/EventController.php
+++ api/controllers/EventController.php
@@ -11,5 +11,128 @@ class EventController
         $this->event = new Event($conn);
     }
 
-    public function 
+    // get all 
+    public function index()
+    {
+        header('Content-Type: aplication/json');
+
+        $events = $this->event->getAll();
+
+        echo json_encode(["status" => "success", "data" => $events]);
+    }
+
+    // get by id
+    public function show()
+    {
+        header('Content-Type: application/json');
+
+        $id = $_GET['id'] ?? null;
+
+        // cek ada id atau engga
+        if(!$id) 
+        {
+            echo json_encode(["status" => "error", "message" => "ID event tidak ada!"]);
+            return;
+        }
+
+        $event = $this->event->getById($id);
+
+        // cek event ada atau engga
+        if(!$event)
+        {
+            echo json_encode(["status" => "error", "message" => "Event tidak ditemukan!"]);
+            return;
+        }
+
+        // sukses
+        echo json_encode(["status" => "success", "data" => $event]);
+    }
+
+    // create 
+    public function store()
+    {
+        header('Content-Type: application/json');
+
+        $name = $_POST['event_name'] ?? '';
+        $location = $_POST['event_location'] ?? '';
+        $date = $_POST['event_date'] ?? '';
+        $quota = $_POST['quota'] ?? 0;
+
+        // ngecek nama lokasi dan tanggal
+        if(!$name || !$location || !$date)
+        {
+            echo json_encode(["status" => "error", "message" => "Event name, location, date tidak ada!"]);
+            return;
+        }
+
+        $result = $this->event->createEvent($name, $location, $date, $quota);
+
+        // ngecek sukses atau engga
+        if($result) 
+        {
+            echo json_encode(["status" => "success", "message" => "Event berhasil dibuat!"]);
+        } 
+        else 
+        {
+            echo json_encode(["status" => "error", "message" => "Gagal membuat event!"]);
+        }
+    }
+
+    // update 
+    public function update()
+    {
+        header('Content-Type: application/json');
+
+        $id = $_POST['event_id'] ?? '';
+        $name = $_POST['event_name'] ?? '';
+        $location = $_POST['event_location'] ?? '';
+        $date = $_POST['event_date'] ?? '';
+        $quota = $_POST['quota'] ?? 0;
+
+        // ngecek id , nama, lokasi, dan tanggal
+        if(!$id || !$name || !$location || !$date) 
+        {
+            echo json_encode(["status" => "error", "message" => "Event name, location, date tidak ada!"]);
+            return;
+        }
+
+        $result = $this->event->updateEvent($id, $name, $location, $date, $quota);
+
+        // cek sukses atau engga
+        if($result) 
+        {
+            echo json_encode(["status" => "success", "message" => "Event berhasil diUpdate!"]);
+        } 
+        else 
+        {
+            echo json_encode(["status" => "error", "message" => "Gagal update event!"]);
+        }
+    }
+
+    // delete
+    public function delete()
+    {
+        header('Content-Type: application/json');
+
+        $id = $_POST['event_id'] ?? null;
+
+        // cek id
+        if(!$id) 
+        {
+            echo json_encode(["status" => "error", "message" => "Id event kosong!"]);
+            return;
+        }
+
+        $result = $this->event->deleteEvent($id);
+
+        // cek sukses atau engga
+        if($result) 
+        {
+            echo json_encode(["status" => "success", "message" => "Event berhasil diDelete!"]);
+        } 
+        else 
+        {
+            echo json_encode(["status" => "error", "message" => "Gagal delete event!"]);
+        }
+    }
 }
